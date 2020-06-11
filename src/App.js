669b8e80891bd1b1312baf5e4cf12a60aeea1e36
Devair Augusto da Silva <53318711+DevairS@users.noreply.github.com>
2020-06-11 14:32:07 -0300

    segundo commit Devair

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Devair lindo
+          MEu piruuuu ta sniper
         </a>
       </header>
     </div>
